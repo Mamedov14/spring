595e9da4cd56e023dcf970018ddebd8081e1ddbb
Vagif <74174732+Mamedov14@users.noreply.github.com>
2022-04-17 13:52:52 +0300

    update
    GetMapping

--- SpringMVS-JDBS/src/main/java/com/example/springmvsjdbs/controller/StudentController.java
+++ SpringMVS-JDBS/src/main/java/com/example/springmvsjdbs/controller/StudentController.java
@@ -1,4 +1,15 @@
 package com.example.springmvsjdbs.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
 public class StudentController {
+
+    @GetMapping("/")
+    public String getHello() {
+        return "hello";
+    }
 }
