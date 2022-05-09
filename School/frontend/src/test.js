9ccb1c934c7afb9890c535063e1ba0fe40852f07
Vagif <74174732+Mamedov14@users.noreply.github.com>
2022-05-09 19:54:59 +0300

    axios config parameters

--- School/frontend/src/test.js
+++ School/frontend/src/test.js
@@ -1,7 +1,12 @@
 const axios = require('axios').default
 
 axios
-    .get('https://fortnite-api.com/v1/banners')
+    .get('https://fortniteapi.io/v2/items/list?lang=en', {
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: 'afcb772b-9087b6e2-c9d08303-587c2bcd',
+        },
+    })
     .then(response => {
         console.log(response.data);
     })
